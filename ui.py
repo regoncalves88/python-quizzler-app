4f4e05023e5e66dfab0c11b43ad5aad6b3f67e8f
Renato Gonçalves <renatogoncalves@Renatos-MacBook-Pro.local>
2021-02-24 18:05:15 -0300

    Implement functionalities to check user answer, call next question and track score.

--- ui.py
+++ ui.py
@@ -20,11 +20,11 @@ class QuizInterface:
         self.canvas.grid(column=0, row=1, columnspan=2, pady=50)
 
         true_image = PhotoImage(file="images/true.png")
-        self.true_button = Button(image=true_image, highlightthickness=0)
+        self.true_button = Button(image=true_image, highlightthickness=0, command=self.check_true)
         self.true_button.grid(column=0, row=2)
 
         false_image = PhotoImage(file="images/false.png")
-        self.false_button = Button(image=false_image, highlightthickness=0)
+        self.false_button = Button(image=false_image, highlightthickness=0, command=self.check_false)
         self.false_button.grid(column=1, row=2)
 
         self.get_next_question()
@@ -32,5 +32,29 @@ class QuizInterface:
         self.window.mainloop()
 
     def get_next_question(self):
-        q_text = self.quiz.next_question()
-        self.canvas.itemconfig(self.question, text=q_text)
+        self.canvas.config(bg="white")
+        self.score.config(text=f"Score: {self.quiz.score}")
+
+        if self.quiz.still_has_questions():
+            q_text = self.quiz.next_question()
+            self.canvas.itemconfig(self.question, text=q_text)
+        else:
+            self.canvas.itemconfig(self.question, text="You've reach the end of the quizz!")
+            self.true_button.config(state="disabled")
+            self.false_button.config(state="disabled")
+
+    def check_true(self):
+        is_right = self.quiz.check_answer("True")
+        self.give_feedback(is_right)
+
+    def check_false(self):
+        is_right = self.quiz.check_answer("False")
+        self.give_feedback(is_right)
+
+    def give_feedback(self, is_right):
+        if is_right:
+            self.canvas.config(bg="green")
+        else:
+            self.canvas.config(bg="red")
+
+        self.window.after(1000, self.get_next_question)
