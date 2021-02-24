0dc04777f78f24a47a1593ece3903259e60f712a
Renato Gonçalves <renatogoncalves@Renatos-MacBook-Pro.local>
2021-02-24 17:36:14 -0300

    Create functionality to retrieve the question information from database and build it in the UI.

--- ui.py
+++ ui.py
@@ -1,10 +1,12 @@
 from tkinter import *
+from quiz_brain import QuizBrain
 
 THEME_COLOR = "#375362"
 
 
 class QuizInterface:
-    def __init__(self):
+    def __init__(self, quiz_brain: QuizBrain):
+        self.quiz = quiz_brain
         self.window = Tk()
         self.window.title("Quizzler App")
         self.window.config(padx=20, pady=20, bg=THEME_COLOR)
@@ -14,7 +16,7 @@ class QuizInterface:
 
         self.canvas = Canvas(width=300, height=250, bg="white")
         self.question = self.canvas.create_text((150, 125), text="Question here!", fill=THEME_COLOR,
-                                                font=("Arial", 20, "italic"))
+                                                width=280, font=("Arial", 20, "italic"))
         self.canvas.grid(column=0, row=1, columnspan=2, pady=50)
 
         true_image = PhotoImage(file="images/true.png")
@@ -25,4 +27,10 @@ class QuizInterface:
         self.false_button = Button(image=false_image, highlightthickness=0)
         self.false_button.grid(column=1, row=2)
 
+        self.get_next_question()
+
         self.window.mainloop()
+
+    def get_next_question(self):
+        q_text = self.quiz.next_question()
+        self.canvas.itemconfig(self.question, text=q_text)
